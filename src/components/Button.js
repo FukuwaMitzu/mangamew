dac99ef50afa53f35f254d4a3c5fb8777068addc
FukuwaMitzu <an01647719638@gmail.com>
2022-04-19 20:43:28 +0700

    Update hiệu ứng button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 export default function Button(props){
     return (
-        <button className="bg-primary text-dominant rounded-xl px-3 py-2">{props.children}</button>
+        <button className="bg-primary text-dominant rounded-xl px-3 py-2 active:bg-primary-dark">{props.children}</button>
     );
 }
